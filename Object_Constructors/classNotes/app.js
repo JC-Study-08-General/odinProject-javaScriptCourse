0f063c7d7dce5a02c4f2d65e983a38faf6c0fda5
Justin Heath <just.coder@outlook.com>
2023-07-18 14:49:18 +0100

    Project 1 complete

---
+++ Object_Constructors/classNotes/app.js
@@ -0,0 +1,60 @@
+// Build the constructor
+function Player(name, marker) {
+  this.name = name
+  this.marker = marker
+  this.sayName = function() {
+    console.log('Example_0', name)
+  }
+}
+
+// Use the constructor
+const player1 = new Player('steve', 'X')
+const player2 = new Player('also steve', 'O')
+player1.sayName() // logs 'steve'
+player2.sayName() // logs 'also steve'
+
+// *** The Prototype *** \\
+// Prototype is an inherited object.
+// Every prototype object inherits from Object.prototype by default!!!!!!!!!
+console.log('Example_1', Player.prototype)
+console.log('Example_2', player1)
+// The return value (result) of this function refers to the .prototype property of the Object Constructor (i.e., Player(name, marker))
+console.log('Example_3', Object.getPrototypeOf(player1)) // Use this method not .__proto__
+// Same As:
+console.log('Example_4', player1.__proto__) // __proto__ this was created as highly unlikely anyone will overwrite this, but is the same as .prototype.
+
+// ### Prototypal Inheritance ###
+// What do we use Prototypes for?
+console.log('Example_5', player1.valueOf())
+// How to find the origins of the prototype:
+console.log('Example_6a', player1.hasOwnProperty('valueOf')) // false
+console.log('Example_6b', Object.prototype.hasOwnProperty('valueOf')) // true
+// The chain does not go on forever:
+console.log('Example_7', Object.getPrototypeOf(Object.prototype)) // null -- end of the line/chain
+
+// ## Object.setPrototypeOf() ##
+// Example - add a Person Object Constructor to the Player example, and making Player inherit from Person!
+
+function Person(name) {
+  this.name = name
+}
+
+Person.prototype.sayName2 = function() {
+  console.log('Example_8a', `Hello, I'm ${this.name}!`)
+}
+
+Player.prototype.getMarker = function() {
+  console.log('Example_8b', `My marker is '${this.marker}'`)
+}
+
+Object.setPrototypeOf(Player.prototype, Person.prototype)
+Object.getPrototypeOf(Player.prototype) // returns Person.prototype NOT Object.prototype now.
+
+player1.sayName() // logs 'steve'
+player2.sayName() // logs 'also steve'
+
+player1.sayName2() // Hello, I'm steve!
+player2.sayName2() // Hello, I'm also steve!
+
+player1.getMarker() // My marker is 'X'
+player2.getMarker() // My marker is 'O'
---
+++ Object_Constructors/project/app.js
@@ -0,0 +1,94 @@
+let myLibrary = [new Book('test','test',10,true), new Book('test2','test2',210,false)];
+console.log(myLibrary)
+
+// Object Constructor
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+
+  this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`
+}
+
+// Form Data
+const form = document.querySelector('form');
+const titleInput = form.elements.title;
+const authorInput = form.elements.author;
+const pagesInput = form.elements.pages;
+const readInput = form.elements.read;
+
+// List items
+const booksPane = document.getElementById('books-pane');
+const allBooks = document.getElementsByClassName('book');
+
+// Listening for the form submit.
+form.addEventListener('submit', (e) => {
+  for (const book of allBooks) {
+    book.classList.remove('just-created');
+  };
+  
+  const newBook = new Book(titleInput.value, authorInput.value, +pagesInput.value, readInput.checked)
+
+  myLibrary.push(newBook)
+
+  createBookList(newBook);
+  form.reset();
+  
+  e.preventDefault();
+});
+
+// Func - Create a list of books
+createBookList = () => {
+  booksPane.innerHTML = '';
+
+  myLibrary.forEach((b, i) => {
+    const textNode = document.createTextNode(b.info());
+    const textSpan = document.createElement('span');
+    textSpan.appendChild(textNode)
+    textSpan.setAttribute('title', 'Delete');
+    textSpan.classList.add('book');
+
+    if(myLibrary.length - 1 === i) {
+      textSpan.classList.add('just-created');
+    }
+    
+    const textLi = document.createElement('li');
+    textLi.appendChild(textSpan);
+  
+    // Add the delete listner
+    textSpan.addEventListener('click', () => {
+      deleteBook(i);
+    });
+
+    if(!b.read) {
+      const readNode = document.createTextNode('read');
+      const readSpan = document.createElement('span');
+      readSpan.appendChild(readNode)
+      readSpan.classList.add('read');
+
+      textLi.appendChild(readSpan);
+
+      // Add the read listner
+      readSpan.addEventListener('click', () => {
+        updateBook(i);
+      });
+    }
+
+    booksPane.appendChild(textLi);
+  })  
+}
+
+// Func - Delete book
+deleteBook = (id) => {
+  myLibrary.splice(id, 1);
+  createBookList();
+}
+
+// Func - Update Read Status
+updateBook = (id) => {
+  myLibrary[id].read = true;
+  createBookList();
+}
+
+createBookList()
